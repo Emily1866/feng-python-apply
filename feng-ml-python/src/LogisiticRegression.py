48066ba3877722816338bcc23aaa307143b73868
jiangfeng07 <1154142410@qq.com>
2017-09-26 14:41:52 +0800

    modify

--- feng-ml-python/src/LogisiticRegression.py
+++ feng-ml-python/src/LogisiticRegression.py
@@ -1,37 +1,102 @@
 # encoding=utf-8
 import numpy as np
+import matplotlib.pyplot as plt
 
 
-def gradAscent(dataMatIn, classLabels):
-    dataMatrix = np.mat(dataMatIn)  # 数据列表转换成矩阵
-    labelMat = np.mat(classLabels).transpose()  # 类标签列表转换成矩阵
-    m, n = np.shape(dataMatrix)  # 得到dataMatrix矩阵大小
-    alpha = 0.001  # 每次上升的步长
-    maxCycles = 500  # 迭代次数
-    weights = np.ones((n, 1))
-    for k in range(maxCycles):
-        h = sigmoid(dataMatrix * weights)  # 计算假设函数h（列向量）
-        error = (labelMat - h)  # 类标签和假设函数的误差
-        weights = weights + alpha * dataMatrix.transpose() * error  # 对weights进行迭代更新
-    return weights
+# sigmoid 函数
+def sigmoid(X):
+    return 1.0 / (1 + np.exp(-X))
 
 
-def sigmoid(inX):
-    return 1.0 / (1 + np.exp(-inX))
+# sigmoid 函数导数
+def sigmoid_derivative(X):
+    Y = sigmoid(X)
+    return np.multiply(Y, 1 - Y)
 
 
-def loadDataSet():
-    dataMat = []
-    labelMat = []
-    fr = open('../resources/lr.txt')
-    for line in fr.readlines():
-        lineArr = line.strip().split()
-        dataMat.append([1.0, float(lineArr[0]), float(lineArr[1])])  # 得到数据列表
-        labelMat.append(int(lineArr[2]))  # 类标签
-    return dataMat, labelMat
+def initialize_parameters(n_x):
+    W = np.zeros((1, n_x))
+    b = np.zeros((1, 1))
+    parameters = {"W": W,
+                  "b": b}
+    return parameters
+
+
+def forward_propagation(X, parameters):
+    Z = np.dot(parameters["W"], X) + parameters["b"]
+    A = sigmoid(Z)
+    cache = {"Z": Z,
+             "A": A}
+    return cache
+
+
+def compute_cost(A, Y):
+    m = Y.shape[1]
+    logprobs = np.multiply(np.log(A), Y) + np.multiply(np.log(1 - A), (1 - Y))
+    cost = (-1 / m) * np.sum(logprobs)
+    cost = np.squeeze(cost)
+
+    return cost
+
+
+def backward_propagation(cache, X, Y):
+    m = X.shape[1]
+    Y = Y.reshape((1, m))
+    A = cache['A']
+    dZ = A - Y
+    dW = (1 / m) * np.dot(dZ, X.T)
+    db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
+    grads = {'dW': dW,
+             'db': db}
+    return grads
+
+
+def update_parameters(parameters, grads, learning_rate=0.02):
+    W = parameters['W']
+    b = parameters['b']
+    dW = grads['dW']
+    db = grads['db']
+    W -= learning_rate * dW
+    b -= learning_rate * db
+    parameters = {'W': W,
+                  'b': b}
+    return parameters
+
+
+def lr_model(X, Y, num_iteration=20000, print_cost=False, seed=3):
+    np.random.seed(seed)
+    # m为样本数，n 为样本维度
+    n_x, m = X.shape
+    parameters = initialize_parameters(n_x)
+    for i in range(num_iteration):
+        cache = forward_propagation(X, parameters)
+        cost = compute_cost(cache['A'], Y)
+        # print(cost)
+
+        grads = backward_propagation(cache, X, Y)
+        parameters = update_parameters(parameters, grads)
+        if print_cost and i % 1000 == 0:
+            print("Cost after iteration %i: %s" % (i, str(cost)))
+    return parameters
 
 
 if __name__ == '__main__':
-    data, lable = loadDataSet()
-    weights = gradAscent(data, lable)
-    print(weights)
+    # re = initialize_parameters(3)
+    # print(re)
+    #
+    # A = np.ones((1, 4)) * 0.5
+    # B = np.ones((1, 4)) * 0.6
+    # print(A)
+    # print(B)
+    # print(compute_cost(A, B))
+    X = np.array([[0.5, 0.5], [0.6, 0.8], [0.4, 0.6], [1.0, 1.4], [1.1, 1.3], [1.2, 1.5]])
+    Y = np.array([[0], [0], [0], [1], [1], [1]])
+    plt.scatter(X[:, 0], X[:, 1], c=Y, s=40)
+    parameters = lr_model(X.T, Y.T, print_cost=True)
+    w1 = parameters['W'][0][0]
+    w2 = parameters['W'][0][1]
+    b = parameters['b'][0][0]
+    x = np.linspace(0, 2)
+    y = -(w1 / w2) * x - b / w2
+    plt.plot(x, y)
+    plt.show()
--- feng-ml-python/src/PageRank.py
+++ feng-ml-python/src/PageRank.py
@@ -4,34 +4,109 @@
 import numpy as np
 
 
-class PageRank(object):
-    def __init__(self, P, V, X):
-        self.damping_factor = 0.85
-        self.P = P
-        self.V = V
-        self.X = X
+# def build_index(links):
+#     website_list = links.keys()
+#     return {website: index for index, website in enumerate(website_list)}
+#
+#
+# def build_transition_matrix(links, index):
+#     total_links = 0
+#     A = np.zeros((len(index), len(index)))
+#     for webpage in links:
+#         # dangling page
+#         if not links[webpage]:
+#             # Assign equal probabilities to transition to all the other pages
+#             A[index[webpage]] = np.ones(len(index)) / len(index)
+#         else:
+#             for dest_webpage in links[webpage]:
+#                 total_links += 1
+#                 A[index[webpage]][index[dest_webpage]] = 1.0 / len(links[webpage])
+#
+#     return A
+#
+#
+# def pagerank(A, eps=0.0001, d=0.85):
+#     P = np.ones(len(A)) / len(A)
+#     while True:
+#         new_P = np.ones(len(A)) * (1 - d) / len(A) + d * A.T.dot(P)
+#
+#         delta = abs((new_P - P).sum())
+#         if delta <= eps:
+#             return new_P
+#         P = new_P
 
-    def page_rank(self):
-        n = len(self.V)
-        avg = np.ones([n, n]) / n
-        A = self.damping_factor * self.P + (1 - self.damping_factor) * avg
-        x = self.X
-        R = A.dot(x)
-        while True:
-            if x.all() == R.all():
-                return R
-            else:
-                x = R
-                R = A * x
+def page_rank(A, eps=0.0001, d=0.85):
+    P = np.ones(len(A)) / len(A)
+    eeT = np.ones([len(A), len(A)])
+    print((1 - d) * eeT / len(A))
+    print(d * A)
+    G = d * A + (1 - d) * eeT / len(A)
+    print("G")
+    print(G)
+    while True:
+        new_P = G.dot(P.T)
+        delta = abs(new_P - P).sum()
+        if delta <= eps:
+            return new_P
+        P = new_P
 
 
 if __name__ == '__main__':
-    data = [[0, 1 / float(2), 1, 0], [1 / 3.0, 0, 0, 1 / 2.0], [1 / 3.0, 0, 0, 1 / 2.0], [1 / 3.0, 1 / 2.0, 0, 0]]
-    P = np.array(data)
-    V = ['a', 'b', 'c', 'd']
-    x = np.array([1.0, 1.0, 1.0, 1.0])
-    pr = PageRank(P, V, x)
-    print(pr.page_rank())
-    x = np.array([1.0 / 4, 1.0 / 4, 1.0 / 4, 1.0 / 4])
-    pr = PageRank(P, V, x)
-    print(pr.page_rank())
+    # links = {
+    #     'webpage-1': set(['webpage-2', 'webpage-4', 'webpage-5', 'webpage-6', 'webpage-8', 'webpage-9', 'webpage-10']),
+    #     'webpage-2': set(['webpage-5', 'webpage-6']),
+    #     'webpage-3': set(['webpage-10']),
+    #     'webpage-4': set(['webpage-9']),
+    #     'webpage-5': set(['webpage-2', 'webpage-4']),
+    #     'webpage-6': set([]),  # dangling page
+    #     'webpage-7': set(['webpage-1', 'webpage-3', 'webpage-4']),
+    #     'webpage-8': set(['webpage-1']),
+    #     'webpage-9': set(['webpage-1', 'webpage-2', 'webpage-3', 'webpage-8', 'webpage-10']),
+    #     'webpage-10': set(['webpage-2', 'webpage-3', 'webpage-8', 'webpage-9']),
+    # }
+    #
+    # website_index = build_index(links)
+    # # print(website_index)
+    #
+    # A = build_transition_matrix(links, website_index)
+    # # print(A)
+    #
+    # results = pagerank(A)
+    #
+    # print("Results:",
+    #       results)  # [ 0.151  0.11355952  0.08725  0.10647619  0.07814286  0.07814286 0.0235  0.07105952  0.15605952  0.13480952]
+    # print(sum(results))  # 1.0
+    # print([item[0] for item in sorted(enumerate(results), key=lambda item: -item[1])])  # [2, 0, 3, 5, 7, 4, 6, 9, 1, 8]
+
+    # M = np.mat([[0, 1, 0, 1, 1, 1, 0, 1, 1, 1],
+    #             [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+    #             [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    #             [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+    #             [0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
+    #             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #             [1, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+    #             [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #             [1, 1, 1, 0, 0, 0, 0, 1, 0, 1],
+    #             [0, 1, 1, 0, 0, 0, 0, 1, 1, 0]])
+    M = np.mat([[0, 1, 1, 1], [1, 0, 0, 1], [0, 0, 0, 0], [0, 1, 1, 0]])
+
+    m, n = M.shape
+    M_avg = np.zeros([m, n])
+
+    sumRow = np.sum(M, axis=1)
+    for i in range(m):
+        sum = sumRow[i, 0]
+        if sum == 0:
+            continue
+        for j in range(n):
+            M_avg[i, j] = M[i, j] / sum
+    print(M_avg)
+    print(M_avg.T)
+    print(page_rank(M_avg.T))
+
+    # M = np.mat([[0.0375, 0.8875, 0.0375, 0.0375],
+    #             [0.4625, 0.0375, 0.0375, 0.4625],
+    #             [0.4625, 0.0375, 0.0375, 0.4625],
+    #             [0.4625, 0.4625, 0.0375, 0.0375]])
+    # p=np.mat([[0.25], [0.25], [0.25], [0.25]])
+    # print(M.dot(p))
